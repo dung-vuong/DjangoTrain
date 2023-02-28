72d93c45779d40db32e5ba0653df8f3f3fe1f9dd
Dung Vuong <dungcoder1213@gmail.com>
2023-02-28 12:38:07 -0800

    crud

--- notes/views.py
+++ notes/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from django.http import Http404
-from django.views.generic import DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView, DeleteView
 from .models import Notes
+from .forms import NotesForm
+
+class NotesDeleteView(DeleteView):
+    model = Notes
+    success_url = '/smart/notes'
+    template_name = 'notes/notes_delete.html'
+
+
+class NotesUpdateView(UpdateView):
+    model = Notes
+    form_class = NotesForm
+    success_url = '/smart/notes'
+
+class NotesCreateView(CreateView):
+    model = Notes
+    form_class = NotesForm
+    success_url = '/smart/notes'
+
 
 class NotesListView(ListView):
     model = Notes
