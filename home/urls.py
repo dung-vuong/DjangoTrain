80d88b4900cb7a39b45586ce7eddd8f86e7717a4
Dung Vuong <dungcoder1213@gmail.com>
2023-03-02 18:47:23 -0800

    update

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('home', views.HomeView.as_view()),
-    path('authorized', views.AuthorizedView.as_view())
+    path('authorized', views.AuthorizedView.as_view()),
+    path('login', views.LoginInterfaceView.as_view()),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 from django.views.generic import CreateView, DetailView, ListView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Notes
 from .forms import NotesForm
 
@@ -19,12 +21,23 @@ class NotesCreateView(CreateView):
     model = Notes
     form_class = NotesForm
     success_url = '/smart/notes'
+    login_url="/admin"
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
 
 
-class NotesListView(ListView):
+class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
     context_object_name = "notes"
     template_name = "notes/notes_list.html"
+    login_url="/admin"
+
+    def get_queryset(self):
+        return self.request.user.notes.all()
 
 class NotesDetailView(DetailView):
     model = Notes
